7ee2b24478350e4dc6d0a33c9442a586a2c5722a
sr <2470498185@qq.com>
2017-12-24 10:05:44 +0800

    main

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "mainwindow.h"
+
+#include <memory>
+#include <QtGui/QApplication>
+#include <logog/include/logog.hpp>
+#include "LogogSimpleFormatter.h"
+#ifdef VTKFBXCONVERTER_FOUND
+#include <fbxsdk.h>
+#include "Common.h"
+FbxManager* lSdkManager = NULL;
+FbxScene* lScene = NULL;
+#endif
+
+#include <vtkSmartPointer.h>
+
+#include "BaseLib/BuildInfo.h"
+#include "VtkVis/VtkConsoleOutputWindow.h"
+
+int main(int argc, char* argv[])
+{
+#ifdef VTKFBXCONVERTER_FOUND
+    InitializeSdkObjects(lSdkManager, lScene);
+#endif
+
+    auto myOutputWindow = vtkSmartPointer<VtkConsoleOutputWindow>::New();
+    vtkOutputWindow::SetInstance(myOutputWindow);
+
+    LOGOG_INITIALIZE();
+    logog::Cout* logogCout = new logog::Cout;
+    BaseLib::LogogSimpleFormatter* formatter = new BaseLib::LogogSimpleFormatter;
+    logogCout->SetFormatter(*formatter);
+    QApplication a(argc, argv);
+    QApplication::setApplicationName("OpenGeoSys - Data Explorer");
+    QApplication::setApplicationVersion(QString::fromStdString(
+        BaseLib::BuildInfo::ogs_version));
+    QApplication::setOrganizationName("OpenGeoSys Community");
+    QApplication::setOrganizationDomain("opengeosys.org");
+    setlocale(LC_NUMERIC,"C");
+    QLocale::setDefault(QLocale::German);
+    std::unique_ptr<MainWindow> w (new MainWindow());
+    w->setWindowTitle( w->windowTitle() + " - " +
+        QString::fromStdString(BaseLib::BuildInfo::git_describe));
+    if (QCoreApplication::arguments().size()>1) {
+        w->loadFileOnStartUp(QCoreApplication::arguments().at(1));
+    }
+    w->show();
+    int returncode = a.exec();
+    delete formatter;
+    delete logogCout;
+    LOGOG_SHUTDOWN();
+#ifdef VTKFBXCONVERTER_FOUND
+    DestroySdkObjects(lSdkManager, true);
+#endif
+
+    return returncode;
+}
